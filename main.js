6df18b7788bc1f694e08d5abe0bfd43da8baf66c
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2025-02-10 12:11:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// Custom cursor
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.querySelector('.cursor');
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-});
-
 // Typewriter effect
 const typewriter = document.querySelector('.typewriter');
 const phrases = [
